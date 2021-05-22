0e0b7ae6b8c9c73c8bed7561b32ebd5316f26330
TheoD02 <theo.d02290@gmail.com>
2021-05-22 02:00:11 +0200

    update

--- public/src/Socket.js
+++
@@ -1,6 +0,0 @@
-import socketIOClient from 'socket.io-client';
-
-const ENDPOINT = 'pyramidsgame.herokuapp.com'
-const socket = socketIOClient(ENDPOINT, {transports: ['websocket']});
-
-export default socket;
