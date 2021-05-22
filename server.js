dd9344e967133b5a9b7472ca12b764a557534ea0
TheoD02 <theo.d02290@gmail.com>
2021-05-22 02:18:55 +0200

    porttest

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 const cors = require('cors');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 const INDEX = '/index.html';
 const { addRoom, getRoom, getAllRooms, deleteRoom, addPlayerToRoom, getPlayerWithRoomCodeAndSocketID, removePlayerFromRoomWithSocketID, initiateGame, searchPlayerInLeavePlayer, playerReJoinGameAfterLeave, deletePlayerFromLeavePlayer, checkUserCanJoinOrReJoinRoom, flipCardInRoom } = require('./src/utils/RoomManager');
 
